23d08c4917cdc6a5b17a8bd8fc1ccde8f5fd4c0d
jayaalipuria <43646976+jayaalipuria@users.noreply.github.com>
2018-09-30 12:47:10 +0530

    Uploaded .py file
    Sending mail after submitting HTML form

---
+++ send_mailfrom_Webpage.py
@@ -0,0 +1,33 @@
+from email.mime.multipart import MIMEMultipart
+#from email.mime.text import MIMEText
+import smtplib
+import cgi
+
+host = 'smtp.gmail.com'
+port = 587
+from_email = "jayalipuria@gmail.com"
+passwrd = "Radhe_Krishna20"
+to_email = "jayalipuria@gmail.com"
+
+email_conn = smtplib.SMTP(host,port)
+email_conn.ehlo()
+email_conn.starttls()
+email_conn.login(from_email, passwrd)
+
+the_msg = MIMEMultipart("alternative")
+the_msg['Subject'] = "Feedback Form"
+the_msg["From"] = from_email
+the_msg["To"] = to_email
+
+
+formData = cgi.FieldStorage()
+d={}
+d[NAME] = formData.getvalue("NAME")
+d[EMAIL] = formData.getvalue("EMAIL")
+d[Feedback] = formData.getvalue("Feedback")
+
+#part1= MIMEText(html_txt,"html")
+#the_msg.attach(part1)
+
+email_conn.sendmail(from_email,to_email,("Feedback from: %s is %s"%(d[NAME],d[Feedback]),"\n Contact by Email: %s"%d[EMAIL]))
+email_conn.quit()
